b8d502ace39c8fbe2d36d2bfd2b1b2b861dc7103
839225516@qq.com <839225516@qq.com>
2018-03-07 14:08:36 +0800

    golang 运算符

---
+++ Go-examples/operator/operator.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//字符串"+" 用作连接符
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
